8c60699d1b538c481aaa381aad0cf17d15a3fb8c
jlarson <jlarson@163.11.236.225>
2017-03-27 19:50:37 -0400

    initial commit

--- pwdcracker/src/pwdcracker/Pwdcracker.java
+++ pwdcracker/src/pwdcracker/Pwdcracker.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package pwdcracker;
+import java.security.MessageDigest; 
+import java.security.NoSuchAlgorithmException;
 
 /**
  *
@@ -15,7 +17,23 @@ public class Pwdcracker {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            
+            // run an update 
+            md.update("HI".getBytes());
+            // get byte data 
+            byte[] mdbytes = md.digest(); 
+            
+            System.out.println(md.toString());
+            System.out.println(mdbytes.toString());
+            
+            
+        }
+        catch (NoSuchAlgorithmException e1) {
+            
+        }
+
     }
     
 }
