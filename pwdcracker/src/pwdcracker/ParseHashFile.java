5ede19289a7299b07f25e3cdcf64f240d30fe838
justinrstallard <justinrstallard@163.11.237.212>
2017-03-27 21:54:41 -0400

    update parsehashfile

--- pwdcracker/src/pwdcracker/ParseHashFile.java
+++ pwdcracker/src/pwdcracker/ParseHashFile.java
@@ -1,11 +1,26 @@
 package pwdcracker;
 
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 /**
  *
  * @author justinrstallard
  */
 public class ParseHashFile {
-    public void parseHashFile(){
+    
+    public static void parseHashFile() throws FileNotFoundException{
+        ArrayList<User> users = new ArrayList<>();
+        Scanner sc = new Scanner(new File("pa3hashes.txt")).useDelimiter(":|\\r\n");
         
+        while(sc.hasNext()){
+            String user = sc.next();
+            String salt = sc.next();
+            String hash = sc.next();
+            
+            users.add(new User(user, salt, hash));
+        }
     }
 }
